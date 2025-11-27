fd97bfe973569fe9783c146b7101e5f5aac57313
Gheeeeeeee <gheoputra55@gmail.com>
2025-11-27 13:15:12 +0700

    make class for offline repositori siswa

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -1,9 +1,20 @@
 package com.example.pertemuan9.repositori
 
 import com.example.pertemuan9.room.Siswa
+import com.example.pertemuan9.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(Siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
 }
