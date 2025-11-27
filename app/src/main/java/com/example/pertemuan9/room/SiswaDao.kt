1ce56c927bc4f1ae60d2057c9a80af0544f79191
Gheeeeeeee <gheoputra55@gmail.com>
2025-11-27 13:02:04 +0700

    make interface for Siswa Dao

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -1,3 +1,13 @@
 package com.example.pertemuan9.room
 
-data class SiswaDao()
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface SiswaDao{
+    
+}
